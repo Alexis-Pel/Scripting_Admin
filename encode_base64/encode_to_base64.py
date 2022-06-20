8ce91bb811f4271f54d34a34bcee8bb4732c2801
tristanlaine <tristan.laine711@gmail.com>
2022-06-20 14:44:32 +0200

    Création fonction user_input

---
+++ encode_base64/encode_to_base64.py
@@ -0,0 +1,15 @@
+
+def user_input():
+    """
+        Function that retrieves a character string from a user.
+        :param: None
+        :return:
+    """
+    input = input("Put your character")
+
+
+
+
+
+
+
