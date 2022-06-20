48957a2e895b64662f111a552312ecbea183f1bd
MaSh4dow <kcoudray2001@gmail.com>
2022-06-20 15:21:32 +0200

    Docstring and TypeHint in decimal_to_binary added

--- decimal_to_binary_utf-8.py
+++ decimal_to_binary_utf-8.py
@@ -1,6 +1,13 @@
 # Transform the numbers on a list to their binary value
 
-def decimal_to_binary(list_utf):
+def decimal_to_binary(list_utf: []):
+
+    """
+    Transform the numbers on a list to their binary value
+    :param list_utf:
+    :return: list_utf transformés en nombres binaires
+    """
+
     for indice, number in enumerate(list_utf):
         list_utf[indice] = bin(number).replace("0b", "")
     return list_utf
