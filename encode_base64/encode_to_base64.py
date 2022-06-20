2d521e9dee1853b59b64c972bfd748c5ab662869
MaSh4dow <kcoudray2001@gmail.com>
2022-06-20 15:33:07 +0200

    file decimal deleted, function paste on encode_to_base64

--- encode_base64/encode_to_base64.py
+++ encode_base64/encode_to_base64.py
@@ -22,3 +22,15 @@ def __base64_string_completion(base64_string: str):
         base64_string += "="
     return base64_string
 
+def decimal_to_binary(list_utf: [int]):
+
+    """
+    Transform the numbers on a list to their binary value
+    :param list_utf:
+    :return: list_utf transformés en nombres binaires
+    """
+
+    for indice, number in enumerate(list_utf):
+        list_utf[indice] = bin(number).replace("0b", "")
+    return list_utf
+
