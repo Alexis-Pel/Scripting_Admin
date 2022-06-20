212f6eaa2e0d081d2e2c5327da18a6dac405dd3a
tristanlaine <tristan.laine711@gmail.com>
2022-06-20 15:19:22 +0200

    Modification fonction user_input

--- encode_base64/encode_to_base64.py
+++ encode_base64/encode_to_base64.py
@@ -2,11 +2,12 @@ def user_input():
     """
         Function that retrieves a character string from a user.
         :param: None
-        :return:
+        :return: user_string
     """
     user_string: str = input("Put your character")
 
     if not user_string.isascii():
+        print("Character not supported, change it !")
         user_input()
 
     return user_string
