342eb23fd978b5d8ca26b40ba35eb07d3bf00995
MaSh4dow <kcoudray2001@gmail.com>
2022-06-22 11:16:16 +0200

    kcoudray_leap-year updated + network_metrics added

--- leap-year/kcoudray_leap-year.py
+++ leap-year/kcoudray_leap-year.py
@@ -1,18 +1,41 @@
+import logging
 import sys
+import argparse
 
-while True:
-    print("Saisir une année")
+logging.basicConfig(level=logging.INFO)
+parser = argparse.ArgumentParser()
+parser.add_argument('-year',
+                    dest='year',
+                    help='Year to test',
+                    type=str,
+                    nargs='+')
+args = parser.parse_args()
 
-    year = int(input())
 
-    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-        print("L'année est bissextile")
-    else:
-        print("L'année n'est pas bissextile")
+def leap_year():
+    """
+    Function that sai if the user input, if it's a number, is a leap year or not
+    :param: None
+    :return:
+    """
+    user_input_list: str = args.year
+    for user_input in user_input_list:
+        if user_input.isdigit():
+            logging.info(f"\n{user_input} is a leap year") if is_leap_year(int(user_input)) else logging.info(
+                f"\n{user_input} is not a leap year")
+        else:
+            logging.warning("Incorrect Input")
+    return
 
-    print("Voulez vous continuer? Tapez Y si vous voulez taper une autre date ou alors n'importe quelle autre touche "
-          "si vous voulez quitter")
-    if input() != 'Y':
-        sys.exit()
-    else:
-        continue
+
+def is_leap_year(year: int):
+    """
+    :param year: Int: Year input:
+    :return: True or False if the year is leap
+    """
+    return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
+
+
+if __name__ == "__main__":
+    leap_year()
+    sys.exit(0)
---
+++ metrics/network_metrics.py
@@ -0,0 +1,36 @@
+import psutil
+
+
+def get_net_io_counters(per_nic : bool = False, no_wrap : bool = True):
+    """
+    Return system-wide network I/O statistics as a named tuple including differents attributes
+    :param per_nic:
+    :param no_wrap:
+    :return: tuple including different attributes
+    """
+    return psutil.net_io_counters(pernic=per_nic, nowrap=no_wrap)
+
+
+def get_net_connections(kind: str = "inet"):
+    """
+    Return system-wide socket connections as a list of named tuples
+    :param kind:
+    :return:
+    """
+    return psutil.net_connections(kind=kind)
+
+
+def get_net_if_addrs():
+    """
+    Return the addresses associated to each NIC installed on the system
+    :return: list of named tuples
+    """
+    return psutil.net_if_addrs()
+
+
+def get_net_if_stats():
+    """
+    Return information about each NIC installed on the system
+    :return: 
+    """
+    return psutil.net_if_stats()
