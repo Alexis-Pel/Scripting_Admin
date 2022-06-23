e12990efc66f9f4f1d4bec3345aa994c2eb1d0f8
tristanlaine <tristan.laine711@gmail.com>
2022-06-23 11:35:10 +0200

    Modification de disk_metrics.py pour prendre en compte l'os utilisé, quasi pas de modif

--- metrics/scripts_metrics/disk_metrics.py
+++ metrics/scripts_metrics/disk_metrics.py
@@ -6,6 +6,7 @@ def show_disk_partitions():
     show alls partitions to disk stockage
     :return: psutil.disk_partitions: [(device=str, mountpoint=str, fstype=str, opts=str, maxfile=int, maxpath=int)]
     """
+    # run for All
     return psutil.disk_partitions()
 
 
@@ -14,6 +15,7 @@ def show_disk_usage(path: str):
     show usage of a disks
     :return: psutil.disk_usage(path): [(total=int, used=int, free=int, percent=float)]
     """
+    # run for All
     return psutil.disk_usage(path)._asdict()
 
 
@@ -22,6 +24,7 @@ def show_disk_io_counters(perdisks: bool):
     show io counters of a disk
     :return: psutil.disk_io_counters(perdisk=False)
     """
+    # run for All
     return psutil.disk_io_counters(perdisk=perdisks)._asdict()
 
 
@@ -34,4 +37,9 @@ def get_all_metrics(show_disk_usage_path: str = "/", show_disk_io_counters_perdi
     """
 
     return {"disk_usage": show_disk_usage(show_disk_usage_path),
-            "disk_io_counter": show_disk_io_counters(show_disk_io_counters_perdisks)}
+            "disk_io_counter": show_disk_io_counters(show_disk_io_counters_perdisks),
+            "disk_partitions": show_disk_partitions()}
+
+
+if __name__ == "__main__":
+    print(get_all_metrics())
