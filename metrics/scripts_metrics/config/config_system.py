d83a541259044b0bc073c697a1b103e9630d092c
alexis.pelissier <alexis.pelissier@edu.itescia.fr>
2022-06-23 12:14:57 +0200

    test

--- metrics/scripts_metrics/config/config_system.py
+++ metrics/scripts_metrics/config/config_system.py
@@ -1,12 +1,11 @@
 import platform
 import logging
 import socket
-logging.basicConfig(level=logging.INFO)
 
 
 def get_my_os():
     os = platform.system()
-    logging.info(f"Operating System: {os}")
+    logging.debug(f"Operating System: {os}")
     return os
 
 
